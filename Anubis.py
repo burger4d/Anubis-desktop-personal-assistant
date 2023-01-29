f2ed124c60fae610d123a1b8587e4c67a1d21bbc
Alex/t3 <104983707+burger4d@users.noreply.github.com>
2023-01-29 10:52:34 +0100

    Update Anubis.py

--- Anubis.py
+++ Anubis.py
@@ -1,7 +1,7 @@
 from common import *
 import commands
 
-say("Bien le bonjour")
+say("Bien le bonjour ô grand maître")
 sleep(2)
 while 1:
     data = stream.read(4096, exception_on_overflow = False)
